63530925172eb1a5fcd57129b41ed93e88d64284
Alastair Maxwell <alastairm@gmail.com>
2017-09-21 12:12:20 +0100

    jesus christ i need some lunch

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, '', 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='generatr',
-    version='0.25',
+    version='0.251',
     description='Dynamic multi-loci/mutli-repeat tract microsatellite sequence generator.',
     long_description=long_description,
     url='https://github.com/helloabunai/RefGeneratr',
